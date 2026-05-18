6af4bbb4d99d2362507d6b4cb9e22f66e6f190b3
iOuter <3155186831@qq.com>
2026-05-19 04:27:39 +0800

    Update HeavenCross.cs

--- LexNinja2Code/Cards/Uncommon/HeavenCross.cs
+++ LexNinja2Code/Cards/Uncommon/HeavenCross.cs
@@ -1,4 +1,5 @@
-﻿using LexNinja2.LexNinja2Code.Api;
+﻿using BaseLib.Utils;
+using LexNinja2.LexNinja2Code.Api;
 using LexNinja2.LexNinja2Code.Api.DynamicVars;
 using LexNinja2.LexNinja2Code.Api.Extensions;
 using LexNinja2.LexNinja2Code.Powers;
@@ -12,26 +13,20 @@ namespace LexNinja2.LexNinja2Code.Cards;
 
 public class HeavenCross() : LexNinja2Card(2, CardType.Power, CardRarity.Uncommon, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<HeavenCrossPower>(1)];
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
         [HoverTipFactory.FromPower<Lexkela>(), HoverTipFactory.FromKeyword(NinjaKeyword.Renshu)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         NinjaAudio.Play("res://LexNinja2/audio/HeavenCross.mp3");
-        new NinjutsuVar(3).ResetToBase();
-        await PowerCmd.Apply<HeavenCrossPower>(
-            new ThrowingPlayerChoiceContext(),
-            Owner.Creature,
-            1,
-            Owner.Creature,
-            null
-        );
+        new NinjutsuVar(3).ResetToBase(); //这是干啥用的？可以删了吗？
+        await CommonActions.ApplySelf<HeavenCrossPower>(choiceContext, this);
     }
 
     protected override void OnUpgrade()
     {
-        this.AddKeyword(CardKeyword.Innate);
+        AddKeyword(CardKeyword.Innate);
     }
 
     public override string CustomPortraitPath => $"HeavenCross_p.png".BigCardImagePath();
