a6fecab9abaf1e3c2f4b354f8a6810ff6fa4049e
iOuter <3155186831@qq.com>
2026-05-19 05:32:03 +0800

    Update PlayHearthStone.cs

--- LexNinja2Code/Cards/Common/PlayHearthStone.cs
+++ LexNinja2Code/Cards/Common/PlayHearthStone.cs
@@ -1,4 +1,5 @@
-﻿using LexNinja2.LexNinja2Code.Api;
+﻿using BaseLib.Utils;
+using LexNinja2.LexNinja2Code.Api;
 using LexNinja2.LexNinja2Code.Api.Extensions;
 using LexNinja2.LexNinja2Code.Powers;
 using MegaCrit.Sts2.Core.Commands;
@@ -21,7 +22,7 @@ public class PlayHearthStone()
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         NinjaAudio.Play("res://LexNinja2/audio/PlayHearthStone.mp3");
-        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, play);
+        await CommonActions.CardBlock(this, play);
     }
 
     protected override void OnUpgrade()
