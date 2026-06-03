b5e07ac9ba9307de96933205833cdcb10bde6433
Flimsyyy <785243793@qq.com>
2026-06-03 22:46:42 +0800

    好好睡可以吃四个火堆生效的遗物了

--- LexNinja2Code/Powers/GoodNightPower.cs
+++ LexNinja2Code/Powers/GoodNightPower.cs
@@ -5,8 +5,13 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Factories;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+using MegaCrit.Sts2.Core.Rewards;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.Runs;
 
 namespace LexNinja2.LexNinja2Code.Powers;
 
@@ -30,6 +35,57 @@ public class GoodNightPower : CustomPowerModel
             return;
         await CreatureCmd.Heal(Owner, Amount);
         Flash();
+        if (Owner.Player.GetRelic<RegalPillow>() != null)
+        {
+            Owner.Player.GetRelic<RegalPillow>().Flash();
+            await CreatureCmd.Heal(Owner, 15);
+        }
+        //if地狱
+        if (Owner.Player.GetRelic<DreamCatcher>() != null)
+        {
+            Owner.Player.GetRelic<DreamCatcher>().Flash();
+            AbstractRoom currentRoom = base.CombatState.RunState.CurrentRoom;
+            if (currentRoom is CombatRoom combatRoom)
+            {
+                combatRoom.AddExtraReward(
+                    base.Owner.Player,
+                    new CardReward(
+                        CardCreationOptions.ForRoom(base.Owner.Player, combatRoom.RoomType),
+                        3,
+                        base.Owner.Player
+                    )
+                );
+            }
+        }
+
+        if (Owner.Player.GetRelic<StoneHumidifier>() != null)
+        {
+            Owner.Player.GetRelic<StoneHumidifier>().Flash();
+            await CreatureCmd.GainMaxHp(Owner, 5);
+        }
+
+        if (Owner.Player.GetRelic<TinyMailbox>() != null)
+        {
+            Owner.Player.GetRelic<TinyMailbox>().Flash();
+            await PotionCmd.TryToProcure(
+                PotionFactory
+                    .CreateRandomPotionInCombat(
+                        base.Owner.Player,
+                        base.Owner.Player.RunState.Rng.CombatPotionGeneration
+                    )
+                    .ToMutable(),
+                base.Owner.Player
+            );
+            await PotionCmd.TryToProcure(
+                PotionFactory
+                    .CreateRandomPotionInCombat(
+                        base.Owner.Player,
+                        base.Owner.Player.RunState.Rng.CombatPotionGeneration
+                    )
+                    .ToMutable(),
+                base.Owner.Player
+            );
+        }
     }
 
     public override async Task AfterSideTurnEnd(
