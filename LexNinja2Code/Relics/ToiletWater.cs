f7f22f45364f68626fb985b713238f0934b15128
Flimsyyy <785243793@qq.com>
2026-06-05 21:24:57 +0800

    修复了花露水一不小心无限消灭诅咒的问题

--- LexNinja2Code/Relics/ToiletWater.cs
+++ LexNinja2Code/Relics/ToiletWater.cs
@@ -7,15 +7,36 @@ using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Rooms;
 
 namespace LexNinja2.LexNinja2Code.Relics;
 
 public class ToiletWater() : LexNinja2Relic
 {
     public override RelicRarity Rarity => RelicRarity.Event;
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1), new("Count", 2)];
+    private int _count;
+    public override bool ShowCounter => true;
+    public override int DisplayAmount => Count;
+    public int Count
+    {
+        get { return _count; }
+        set
+        {
+            AssertMutable();
+            _count = value;
+            UpdateDisplay();
+        }
+    }
 
-    public override Task AfterCardDrawn(
+    public override async Task AfterRoomEntered(AbstractRoom room)
+    {
+        if (room is not CombatRoom)
+            return;
+        Count = DynamicVars["Count"].IntValue;
+    }
+
+    public override async Task AfterCardDrawn(
         PlayerChoiceContext choiceContext,
         CardModel card,
         bool fromHandDraw
@@ -23,17 +44,34 @@ public class ToiletWater() : LexNinja2Relic
     {
         if (card.Owner != base.Owner)
         {
-            return Task.CompletedTask;
+            return;
         }
         if (card.Type != CardType.Curse)
         {
-            return Task.CompletedTask;
+            return;
+        }
+        if (Count == 0)
+        {
+            return;
         }
         NinjaAudio.Play("res://LexNinja2/audio/ToiletWater.mp3");
         Flash();
-        CardCmd.Exhaust(choiceContext, card);
-        CardPileCmd.Draw(choiceContext, Owner);
-        return Task.CompletedTask;
+        await CardCmd.Exhaust(choiceContext, card);
+        await CardPileCmd.Draw(choiceContext, Owner);
+        Count--;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        if (Count == 0)
+        {
+            Status = RelicStatus.Disabled;
+        }
+        else
+        {
+            Status = RelicStatus.Active;
+        }
     }
 
     public override string PackedIconPath => "ToiletWater.png".RelicImagePath();
