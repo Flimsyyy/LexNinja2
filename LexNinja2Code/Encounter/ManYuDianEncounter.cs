34fde083e50cf0d36ab68c628ae70d9e7a1258f2
iOuter <3155186831@qq.com>
2026-06-25 15:04:26 +0800

    encounter

--- LexNinja2Code/Encounter/ManYuDianEncounter.cs
+++ LexNinja2Code/Encounter/ManYuDianEncounter.cs
@@ -1,18 +1,21 @@
-﻿using BaseLib.Abstracts;
-using MegaCrit.Sts2.Core.Models;
+﻿using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Monsters;
 using MegaCrit.Sts2.Core.Rooms;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
 
 namespace LexNinja2.LexNinja2Code.Encounter;
 
-public class ManYuDianEncounter() : CustomEncounterModel(RoomType.Monster)
+[RegisterGlobalEncounter]
+public class ManYuDianEncounter : ModEncounterTemplate
 {
     public override bool IsValidForAct(ActModel act) => false;
 
     public override IEnumerable<MonsterModel> AllPossibleMonsters =>
         [ModelDb.Monster<TerrorEel>(), ModelDb.Monster<PhantasmalGardener>()];
 
-    public override string? CustomScenePath => "res://LexNinja2/scenes/man_yu_dian_encounter.tscn";
+    public override string? CustomEncounterScenePath =>
+        "res://LexNinja2/scenes/man_yu_dian_encounter.tscn";
 
     public override IReadOnlyList<string> Slots => ["first", "second", "third"];
 
@@ -22,4 +25,6 @@ public class ManYuDianEncounter() : CustomEncounterModel(RoomType.Monster)
             (ModelDb.Monster<PhantasmalGardener>().ToMutable(), "second"),
             (ModelDb.Monster<PhantasmalGardener>().ToMutable(), "third"),
         ];
+
+    public override RoomType RoomType => RoomType.Monster;
 }
