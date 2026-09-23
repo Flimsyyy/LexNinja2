ab7b76d1aa07a6c86bdc9b7b0c306affa178a544
iOuter <3155186831@qq.com>
2026-09-23 13:03:48 +0800

    use calcaute var in inferno dragon

--- LexNinja2Code/Powers/InfernoDragonPower.cs
+++ LexNinja2Code/Powers/InfernoDragonPower.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using BaseLib.Cards.Variables;
 using LexNinja2.LexNinja2Code.Api;
 using LexNinja2.LexNinja2Code.Api.Extensions;
 using LexNinja2.LexNinja2Code.Api.Powers;
@@ -14,19 +15,28 @@ namespace LexNinja2.LexNinja2Code.Powers;
 
 public class InfernoDragonPower : LexNinja2Power
 {
+    private const string BuffAmount = "BuffAmount";
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new("dian", 0)];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [
+            new(BuffAmount + "Base", 0),
+            new(BuffAmount + "Extra", 1),
+            new CustomCalculatedVar(BuffAmount).WithMultiplier(
+                (power, _) => CalculateExtraDamage(power)
+            ),
+        ];
     protected override IEnumerable<IHoverTip> AdditionalHoverTips => [LexKela.HoverTip()];
 
     public override string CustomIconPath => "InfernoDragonPower.png".PowerImagePath();
     public override string? CustomBigIconPath => "InfernoDragonPower.png".BigPowerImagePath();
 
-    private decimal CalculateExtraDamage()
+    private static decimal CalculateExtraDamage(PowerModel powerModel)
     {
-        decimal kela = LexKela.Get(Owner.Player!);
-        DynamicVars["dian"].BaseValue = Amount * kela;
-        return DynamicVars["dian"].BaseValue;
+        decimal kela = powerModel is { IsMutable: true, Owner.Player: not null }
+            ? LexKela.Get(powerModel.Owner.Player)
+            : 1;
+        return powerModel.Amount * kela;
     }
 
     public override decimal ModifyDamageMultiplicative(
@@ -37,7 +47,7 @@ public class InfernoDragonPower : LexNinja2Power
         CardModel? cardSource
     )
     {
-        var extraDamage = CalculateExtraDamage() / 100;
+        var extraDamage = CalculateExtraDamage(this) / 100;
         return
             dealer != Owner && !Owner.Pets.Contains(dealer)
             || !props.IsPoweredAttack()
